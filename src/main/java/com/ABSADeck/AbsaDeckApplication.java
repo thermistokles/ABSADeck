14f1b8761d78c46da976257030333aca6a032a72
thermistokles <ameydsmore@gmail.com>
2020-02-05 21:22:18 +0530

    Base Project

---
+++ src/main/java/com/ABSADeck/AbsaDeckApplication.java
@@ -0,0 +1,13 @@
+package com.ABSADeck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsaDeckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbsaDeckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ABSADeck/AbsaDeckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ABSADeck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbsaDeckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
